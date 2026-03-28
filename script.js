d71c5868d87e3f8b49c42f3119597b5d2471cf75
Fabio Oliveira <fabio.oliveyra@gmail.com>
2026-03-27 22:27:34 -0300

    tabuada

---
+++ script.js
@@ -0,0 +1,15 @@
+function gerarTabuada(){
+
+let numero = document.getElementById("numero").value;
+
+let resultado = document.getElementById("resultado");
+
+resultado.innerHTML = "";
+
+for(let i = 1; i <= 10; i++){
+
+resultado.innerHTML += numero + " x " + i + " = " + (numero * i) + "<br>";
+
+}
+
+}
